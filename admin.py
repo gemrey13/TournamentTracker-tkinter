dc237a13c9204d1ffdce24295c599e321ad05fbf
gemrey13 <gemreyranola@gmail.com>
2024-02-23 09:01:44 +0800

    build: added sidebarframe

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 import customtkinter
 from PIL import Image
 import os
-from adminUI.SidebarFrame import HomeFrame, SecondFrame, ThirdFrame
+from adminUI.SidebarFrame import HomeFrame, SecondFrame, ThirdFrame, Sidebar
 
 
 class AdminWindow(customtkinter.CTkToplevel):
@@ -12,9 +12,11 @@ class AdminWindow(customtkinter.CTkToplevel):
         self.after(250, lambda: self.iconbitmap("./images/run.ico"))
         self.resizable(False, False)
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
 
+        self.config(width=100)
         images_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(images_path, "run.ico")), size=(26, 26))
 
@@ -28,105 +30,112 @@ class AdminWindow(customtkinter.CTkToplevel):
             light_image=Image.open(os.path.join(images_path, "add_user_dark.png")), dark_image=Image.open(os.path.join(images_path, "add_user_light.png")), size=(20, 20)
         )
 
-        self.navigation_frame_label = customtkinter.CTkLabel(
-            self, text="  Image Example", image=self.logo_image, compound="left", font=customtkinter.CTkFont(size=15, weight="bold")
-        )
-        self.navigation_frame_label.grid(row=0, column=0, padx=10, pady=20)
+        self.sidebar_frame = Sidebar(self, width=100, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
-        self.home_frame = HomeFrame(self, corner_radius=0, fg_color="transparent")
-        self.home_frame.grid_columnconfigure(0, weight=1)
+    #     self.navigation_frame_label = customtkinter.CTkLabel(
+    #         self, text="  Image Example", image=self.logo_image, compound="left", font=customtkinter.CTkFont(size=15, weight="bold")
+    #     )
+    #     self.navigation_frame_label.grid(row=0, column=0, padx=10, pady=20)
 
-        self.second_frame = SecondFrame(self, corner_radius=0, fg_color="transparent")
-        self.third_frame = ThirdFrame(self, corner_radius=0, fg_color="transparent")
+    #     self.home_frame = HomeFrame(self, corner_radius=0, fg_color="transparent")
+    #     self.home_frame.grid_columnconfigure(0, weight=1)
 
-        self.home_button = customtkinter.CTkButton(
-            self,
-            corner_radius=0,
-            height=40,
-            border_spacing=10,
-            text="Home",
-            fg_color="transparent",
-            text_color=("gray10", "gray90"),
-            hover_color=("gray70", "gray30"),
-            image=self.home_image,
-            anchor="w",
-            command=self.home_button_event,
-        )
-        self.home_button.grid(row=1, column=0, padx=20, pady=5, sticky="ew")
+    #     self.second_frame = SecondFrame(self, corner_radius=0, fg_color="transparent")
+    #     self.third_frame = ThirdFrame(self, corner_radius=0, fg_color="transparent")
 
-        self.frame_2_button = customtkinter.CTkButton(
-            self,
-            corner_radius=0,
-            height=40,
-            border_spacing=10,
-            text="Frame 2",
-            fg_color="transparent",
-            text_color=("gray10", "gray90"),
-            hover_color=("gray70", "gray30"),
-            image=self.chat_image,
-            anchor="w",
-            command=self.frame_2_button_event,
-        )
-        self.frame_2_button.grid(row=2, column=0, padx=20, pady=5, sticky="ew")
+    #     self.home_button = customtkinter.CTkButton(
+    #         self,
+    #         corner_radius=0,
+    #         height=40,
+    #         border_spacing=10,
+    #         text="Home",
+    #         fg_color="transparent",
+    #         text_color=("gray10", "gray90"),
+    #         hover_color=("gray70", "gray30"),
+    #         image=self.home_image,
+    #         anchor="w",
+    #         command=self.home_button_event,
+    #     )
+    #     self.home_button.grid(row=1, column=0, padx=20, pady=5, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(
-            self,
-            corner_radius=0,
-            height=40,
-            border_spacing=10,
-            text="Frame 3",
-            fg_color="transparent",
-            text_color=("gray10", "gray90"),
-            hover_color=("gray70", "gray30"),
-            image=self.add_user_image,
-            anchor="w",
-            command=self.frame_3_button_event,
-        )
-        self.frame_3_button.grid(row=3, column=0, padx=20, pady=5, sticky="ew")
+    #     self.frame_2_button = customtkinter.CTkButton(
+    #         self,
+    #         corner_radius=0,
+    #         height=40,
+    #         border_spacing=10,
+    #         text="Frame 2",
+    #         fg_color="transparent",
+    #         text_color=("gray10", "gray90"),
+    #         hover_color=("gray70", "gray30"),
+    #         image=self.chat_image,
+    #         anchor="w",
+    #         command=self.frame_2_button_event,
+    #     )
+    #     self.frame_2_button.grid(row=2, column=0, padx=20, pady=5, sticky="ew")
 
-        self.logout_brn = customtkinter.CTkButton(self, text="logout", command=self.logout)
-        self.logout_brn.grid(row=4, column=0, padx=20, pady=20, sticky="ew")
+    #     self.frame_3_button = customtkinter.CTkButton(
+    #         self,
+    #         corner_radius=0,
+    #         height=40,
+    #         border_spacing=10,
+    #         text="Frame 3",
+    #         fg_color="transparent",
+    #         text_color=("gray10", "gray90"),
+    #         hover_color=("gray70", "gray30"),
+    #         image=self.add_user_image,
+    #         anchor="w",
+    #         command=self.frame_3_button_event,
+    #     )
+    #     self.frame_3_button.grid(row=3, column=0, padx=20, pady=5, sticky="ew")
 
-        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self, values=["Dark", "Light", "System"], command=self.change_appearance_mode_event)
-        self.appearance_mode_menu.grid(row=5, column=0, padx=20, pady=20, sticky="ew")
+    #     self.logout_brn = customtkinter.CTkButton(self, text="logout", command=self.logout)
+    #     self.logout_brn.grid(row=4, column=0, padx=20, pady=20, sticky="ew")
 
-        self.select_frame_by_name("home")
+    #     self.appearance_mode_menu = customtkinter.CTkOptionMenu(self, values=["Dark", "Light", "System"], command=self.change_appearance_mode_event)
+    #     self.appearance_mode_menu.grid(row=5, column=0, padx=20, pady=20, sticky="ew")
 
-    def home_button_event(self):
-        self.select_frame_by_name("home")
+    #     self.select_frame_by_name("home")
 
-    def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
+    # def home_button_event(self):
+    #     self.select_frame_by_name("home")
 
-    def frame_3_button_event(self):
-        self.select_frame_by_name("frame_3")
+    # def frame_2_button_event(self):
+    #     self.select_frame_by_name("frame_2")
 
-    def select_frame_by_name(self, name):
-        # set button color for selected button
-        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
-        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+    # def frame_3_button_event(self):
+    #     self.select_frame_by_name("frame_3")
 
-        # show selected frame
-        if name == "home":
-            self.home_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.home_frame.grid_forget()
-        if name == "frame_2":
-            self.second_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.second_frame.grid_forget()
-        if name == "frame_3":
-            self.third_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.third_frame.grid_forget()
+    # def select_frame_by_name(self, name):
+    #     # set button color for selected button
+    #     self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+    #     self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+    #     self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
 
-    def logout(self):
-        self.master.logout()
-        self.destroy()
+    #     # show selected frame
+    #     if name == "home":
+    #         self.home_frame.grid(
+    #             row=0,
+    #             column=1,
+    #         )
+    #     else:
+    #         self.home_frame.grid_forget()
+    #     if name == "frame_2":
+    #         self.second_frame.grid(row=0, column=1, sticky="nsew")
+    #     else:
+    #         self.second_frame.grid_forget()
+    #     if name == "frame_3":
+    #         self.third_frame.grid(row=0, column=1, sticky="nsew")
+    #     else:
+    #         self.third_frame.grid_forget()
 
-    def change_appearance_mode_event(self, new_appearance_mode):
-        customtkinter.set_appearance_mode(new_appearance_mode)
+    # def logout(self):
+    #     self.master.logout()
+    #     self.destroy()
+
+    # def change_appearance_mode_event(self, new_appearance_mode):
+    #     customtkinter.set_appearance_mode(new_appearance_mode)
 
 
 class TabView(customtkinter.CTkTabview):
--- adminUI/SidebarFrame.py
+++ adminUI/SidebarFrame.py
@@ -3,15 +3,64 @@ import customtkinter
 from PIL import Image
 
 
+class Sidebar(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        images_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../images")
+        self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(images_path, "run.ico")), size=(26, 26))
+
+        self.logo_label = customtkinter.CTkLabel(
+            self, image=self.logo_image, compound="left", text="  Tournament Tracker", font=customtkinter.CTkFont(size=15, weight="bold")
+        )
+        self.logo_label.grid(row=0, column=0, padx=20, pady=30)
+
+        self.add_team_btn = customtkinter.CTkButton(self, text="Add Team", command=self.sidebar_button_event)
+        self.add_team_btn.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
+        self.add_tournament_btn = customtkinter.CTkButton(self, text="Add Tournament", command=self.sidebar_button_event)
+        self.add_tournament_btn.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
+
+        self.add_sport_btn = customtkinter.CTkButton(self, text="Add Sport", command=self.sidebar_button_event)
+        self.add_sport_btn.grid(row=3, column=0, padx=20, pady=10)
+
+        self.string_input_button = customtkinter.CTkButton(self, text="Open CTkInputDialog", command=self.open_input_dialog_event)
+        self.string_input_button.grid(row=4, column=0, padx=20, pady=(10, 10))
+
+        self.appearance_mode_label = customtkinter.CTkLabel(self, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+
+        self.logout_btn = customtkinter.CTkButton(self, text="logout", command=self.logout)
+        self.logout_btn.grid(row=7, column=0, padx=20, pady=20, sticky="ew")
+
+    def logout(self):
+        self.master.master.logout()
+        self.master.destroy()
+
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        print("CTkInputDialog:", dialog.get_input())
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+
 class HomeFrame(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.configure(width=50)
         images_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../images")
         self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(images_path, "image_icon_light.png")), size=(20, 20))
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(images_path, "large_test_image.png")), size=(500, 150))
 
+        self.add_team_btn = customtkinter.CTkButton(self, text="add team", compound="right")
+        self.add_team_btn.grid(row=0, column=1)
+
         self.home_label = customtkinter.CTkLabel(self, text="", image=self.large_test_image)
-        self.home_label.grid(row=0, column=0, padx=20, pady=10)
+        # self.home_label.grid(row=0, column=0, padx=20, pady=10)
 
         self.home_frame_button_1 = customtkinter.CTkButton(self, text="", image=self.image_icon_image)
         self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
